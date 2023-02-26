d650771226a522e48cafe3ae0852114e5c15ded3
JonobaneL <jonobane@gmail.com>
2023-02-26 23:04:29 +0200

    add responsive navigaton for mobile version

--- src/components/UI/burgerMenu/burgerNav.jsx
+++ src/components/UI/burgerMenu/burgerNav.jsx
@@ -1,21 +1,24 @@
 import SInput from '../input/SInput';
 import styles from './burgerNav.module.scss'
-import searchIcon from '../../../images/dark-icons/search-icon.png'
-const BurgerNav = () => {
-    return <div className={styles.burgerNav}>
-        <div className={styles.content}>
-            <div className={styles.search}>
-                <SInput type="text" placeholder="Find Something Special"/>
-                <img src={searchIcon} alt="search" />
-            </div>
-            <ul className={styles.nav}>
-                <li className={styles.navItem}>Men</li>
-                <li className={styles.navItem}>Women</li>
-                <li className={styles.navItem}>Accessories</li>
-                <li className={styles.navItem}>Sale</li>
-            </ul>
-        </div>
-    </div>;
+import searchIcon from '../../../images/white-icons/search-icon.png'
+
+const BurgerNav = ({visible}) => {
+    const rootClass = [styles.burgerNav]
+    if(visible) rootClass.push(styles.active)
+    return  <div className={rootClass.join(' ')}>
+                <div className={styles.content}>
+                    <div className={styles.search}>
+                        <SInput type="text" placeholder="Find Something Special" data-theme="white"/>
+                        <img src={searchIcon} alt="search" />
+                    </div>
+                    <ul className={styles.nav}>
+                        <li className={styles.navItem}>Men</li>
+                        <li className={styles.navItem}>Women</li>
+                        <li className={styles.navItem}>Accessories</li>
+                        <li className={styles.navItem}>Sale</li>
+                    </ul>
+                </div>
+            </div>;
 }
  
 export default BurgerNav;
