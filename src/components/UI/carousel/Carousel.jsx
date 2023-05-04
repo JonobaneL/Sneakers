f5f7a2f7df827dffa4f3faf4a2ae154fe095842a
JonobaneL <jonobane@gmail.com>
2023-05-04 20:03:22 +0300

    modified project

--- src/components/UI/carousel/Carousel.jsx
+++ src/components/UI/carousel/Carousel.jsx
@@ -4,12 +4,10 @@ import { AnimatePresence, wrap,motion, AnimateSharedLayout } from 'framer-motion
 import { Link } from 'react-router-dom';
 const Carousel = ({data,arrows=false,dots=true,children}) => {
     console.log(children)
-    const primatyCarouselStyles = {
-        padding:`0 ${arrows?'50px':0} ${dots?'30px':0} ${arrows?'50px':0}`
-    }
+   
     const [[page,direction],setPage]=useState([0,0])
     const pageIndex = wrap(0,children.length,page);
-
+    const secondPage = wrap(0,children.length,page+1);
     const arrowsPaginate = newDirection =>{
         setPage([page+newDirection,newDirection])
     }
@@ -19,19 +17,19 @@ const Carousel = ({data,arrows=false,dots=true,children}) => {
     const itemVariants = {
         enter: (direction) => {
             return {
-            //   x: direction > 0 ? 500 : -500,
+              x: direction > 0 ? 100 : -100,
               opacity: 0
             };
           },
           center: {
             // zIndex: 1,
-            // x: 0,
+            x: 0,
             opacity: 1
           },
           exit: (direction) => {
             return {
             //   zIndex: 0,
-            //   x: direction < 0 ? 500 : -500,
+              x: direction < 0 ? 100 : -100,
               opacity: 0
             };
           }
@@ -67,23 +65,47 @@ const Carousel = ({data,arrows=false,dots=true,children}) => {
     }
     return (
         <div className={styles.carousel}>
+                <div className={styles.container}>
             <AnimatePresence initial={false} mode='wait' custom={direction}>
-                <motion.div 
-                key={page}
-                custom={direction}
-                initial="enter"
-                animate="center"
-                exit="exit"
-                variants={itemVariants}
-                transition={{
-                    opacity: { duration: 0.2 },
-                    when:'beforeChildren'
-                }}
-                className={styles.content}>
+                    <motion.div 
+                    key={page}
+                    custom={direction}
+                    initial="enter"
+                    animate="center"
+                    exit="exit"
+                    variants={itemVariants}
+                    transition={{
+                        opacity: { duration: 0.2 },
+                        x: { type: "spring", stiffness: 300, damping: 30,duration:1},
+                        when:'beforeChildren'
+                    }}
+                    className={styles.item}>
+                        
+                        {children[pageIndex]}
+                    </motion.div>
                     
-                    {children[pageIndex]}
-                </motion.div>
             </AnimatePresence>
+            <AnimatePresence initial={false} mode='wait' custom={direction}>
+                    <motion.div 
+                    key={page}
+                    custom={direction}
+                    initial="enter"
+                    animate="center"
+                    exit="exit"
+                    variants={itemVariants}
+                    transition={{
+                        opacity: { duration: 0.2 },
+                        x: { type: "spring", stiffness: 300, damping: 30,duration:1},
+                        when:'beforeChildren'
+                    }}
+                    className={styles.item}>
+                        
+                        {children[secondPage]}
+                    </motion.div>
+                    
+            </AnimatePresence>
+            </div>
+
             <div onClick={()=>arrowsPaginate(-1)} className={styles.leftArrow}>&#60;</div>
             <div onClick={()=>arrowsPaginate(1)} className={styles.rightArrow}>&#62;</div>
             {
@@ -91,10 +113,7 @@ const Carousel = ({data,arrows=false,dots=true,children}) => {
                     {data.map((_,index)=>
                         <motion.div
                         key={index}
-                        // whileHover={{
-                        //     background:"#1d2d44"
-                        // }}
-                        // animate={{boxShadow:pageIndex===index?'0px 0px 0px 2px #1d2d44':'0px 0px 0px 2px transparent'}}
+                        imate={{boxShadow:pageIndex===index?'0px 0px 0px 2px #1d2d44':'0px 0px 0px 2px transparent'}}
                         className={`${styles.dot} ${pageIndex===index?styles.active:''}`}
                         onClick={()=>dotsPaginate(index)}
                     ></motion.div>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,13 @@
 import React,{useState,useEffect,useMemo, useCallback, useRef} from 'react'
 import styles from './about.module.scss'
-import { AnimatePresence, delay, motion, useAnimate } from 'framer-motion';
+import { AnimatePresence, AnimateSharedLayout, delay, motion, useAnimate } from 'framer-motion';
 import sneker from '../images/sneakers.png'
 import boot from '../images/boot.png'
 import sandal from '../images/sandal.png'
 import Carousel from '../components/UI/carousel/Carousel';
+import { LayoutGroup } from 'framer-motion';
 const About = () => {
- 
+  const [position,setPosition] = useState(0)
   const home_data= [
     {id:1,season:'summer 2023',title:"Hello new season",description:'Limited time to offer - up to 50% off',link:"/shoes/men",img:sneker},
     {id:2,season:'summer 2023',title:"reach the tops",description:'convenient shoes for you trip',link:"/shoes/men",img:boot},
@@ -21,18 +22,57 @@ const About = () => {
     alignitems:'center',
     justifyContent:'center',
   }
-  
+  const [items,setItems] = useState([])
+  const addEvent = ()=>{
+    let ran = Math.floor(Math.random() * (100 - 2 + 1)) + 2;
+    setItems(p=>[...p,{id:ran}])
+  }
+  const removeEvent =()=>{
+    const newItems = [...items]
+    newItems.splice(0,1)
+    setItems(newItems)
+  }
     return <div className={styles.about}>
         <div className={styles.content} >
-          <div className={styles.carousel} style={{width:'100%',marginInline:'auto'}}>
+          {/* <div className={styles.carousel} style={{width:'100%',marginInline:'auto'}}> */}
             
-            <Carousel data={home_data}>
+            {/* <Carousel data={home_data}>
               <div style={{...block,background:"#8b1919"}}>Slide 1</div>
               <div style={{...block,background:"#4c8b19"}}>Slide 2</div>
               <div style={{...block,background:"#19838b"}}>Slide 3</div>
-            </Carousel>
+              <div style={{...block,background:"#945821"}}>Slide 4</div>
+              <div style={{...block,background:"#115c14"}}>Slide 5</div>
+              <div style={{...block,background:"#15115c"}}>Slide 6</div>
+            </Carousel> */}
+           
+              
+              <ul className={styles.list}>
+              <AnimatePresence mode="popLayout">
+                {items.map((item,index) => (
+                  <motion.li
+                    className={styles.listItem}
+                    layout
+                    // initial={{ x: 20, opacity: 0 }}
+                    animate={{ translateX: 0, opacity: 1 }}
+                    exit={{ translateX: -200, opacity: 0 }}
+                    transition={{ type: "spring",duration:2 }}
+                    key={index}
+                    onClick={removeEvent}
+                  />
+                ))}
+              </AnimatePresence>
+      </ul>
+            <div style={{display:'flex',gap:'10px',margin:'10px'}}>
+              <button
+                onClick={addEvent}
+              className={styles.btn1}>Click 1</button>
+              <button
+                onClick={removeEvent}
+                className={styles.btn2}>Click 2</button>
+            </div>
+            
           </div>
-        </div>
+        {/* </div> */}
     </div>;
 }
  
