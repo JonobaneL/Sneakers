fa7d0824b712272d556ee9b93d6d8e46e6c57ec0
JonobaneL <jonobane@gmail.com>
2023-08-21 01:47:02 +0300

    modified project

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,12 +7,17 @@ import RadioList from '../components/RadioList/RadioList';
 import RadioButton from '../components/UI/radioButton/RadioButton';
 import searchIcon from '../images/header-icons/search-icon.svg'
 import { Link } from 'react-router-dom';
+import { motion, useAnimationControls } from 'framer-motion';
 const About = () => {
     const searchRef = useRef('')
     const [isOpen,setIsOpen] = useState(false)
+    const controls = useAnimationControls();
+    const searchVariants = {
+        width:250,
+    }
     return <div className={styles.about}>
         <div className={styles.content} >
-            <div className={`${styles.wrapper} ${isOpen?styles.active:''}`}>
+            {/* <div className={`${styles.wrapper} ${isOpen?styles.active:''}`}>
                 <div className={styles.b}/>
                 <ul className={styles.nav}>
                     <li className={styles.nav__item}><Link to={`/shoes/men`}> Men</Link></li>
@@ -34,9 +39,46 @@ const About = () => {
                 <button
                     onClick={()=>setIsOpen(false)}
                     className={styles.cancel}>Cancel</button>
+            </div> */}
+            
+            <div className={styles.header}>
+                <div className={styles.logo}>SNEAKERS</div>
+                <ul className={styles.nav}>
+                    <li className={styles.nav__item}><Link to={`/shoes/men`}> Men</Link></li>
+                    <li className={styles.nav__item}><Link to={`/shoes/women`}> Women</Link></li>
+                    <li className={styles.nav__item}><Link to={`/shoes/kids`}> Kids</Link></li>
+                    <li className={styles.nav__item}><Link to={`/accessories`}> Accessories</Link></li>
+                    <li className={styles.nav__item}><Link to={`/sale`}> Sale</Link></li>
+                </ul>
+                <motion.div 
+                    initial={{
+                        width:180
+                    }}
+                    animate={controls}
+                    transition={{
+                        duration:0.3
+                    }}
+                    whileTap={{
+                        width:250,
+                    }}
+                    className={styles["search-wrapper"]}
+                    
+                >
+                    <button className={styles.search__btn} onClick={()=>controls.start(searchVariants)}>
+                        <img src={searchIcon} alt="search" />
+                    </button>
+                    <input onFocus={()=>setIsOpen(true)} className={styles.search__field} ref={searchRef} type="text" placeholder='Search...' />
+                </motion.div>
+                <div className={styles["button-bar"]}>
+                    <button></button>
+                    <button></button>
+                </div>
+                <button
+                    onClick={()=>setIsOpen(false)}
+                    className={styles.cancel}>
+                        Cancel
+                </button>
             </div>
-            
-            
         </div>
     </div>;
 }
